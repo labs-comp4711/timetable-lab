dcd4f5b975bf9e8db939f49dd3a0f54d730fa8c1
Oscar Kwan <okwan93@gmail.com>
2016-03-08 13:54:43 -0800

    init commit

---
+++ application/views/homepage.php
@@ -0,0 +1,10 @@
+<div class="row">
+    <h2>Burger Bonanza Orders</h2>
+
+    {orders}
+    <p><a href="/welcome/order/{filename}">{filename}</a></p>
+    {/orders}
+    
+    <p>Select an order from the list above to see its receipt.</p>
+        
+</div>
---
+++ application/views/justone.php
@@ -0,0 +1,6 @@
+
+    <h2>{filename} for {customer} ({ordertype})</h2>
+    {delivery}
+    {special}
+    {details}
+    <h3>Order total: {bigbucks}</h3>
