047ee1934482d6cd8df09d70700edd0c3ae04d64
Thomas <thomas__yu@hotmail.com>
2016-03-27 15:30:21 -0700

    Check for match and display bingo along with search results implemented

---
+++ application/views/searchresults.php
@@ -0,0 +1,34 @@
+<h1>BINGO! The Results Match!</h1>
+
+<h3>Facet search results for Days</h3>
+{dayinfo}
+    {day}
+    {start}
+    {coursename}
+    {instructor}
+    {location}
+    {classtype}
+    </br>
+{/dayinfo}
+
+<h3>Facet search results for Timeslots</h3>
+{timeinfo}
+    {day}
+    {start}
+    {coursename}
+    {instructor}
+    {location}
+    {classtype}
+    </br>
+{/timeinfo}
+
+<h3>Facet search results for Courses</h3>
+{courseinfo}
+    {day}
+    {start}
+    {coursename}
+    {instructor}
+    {location}
+    {classtype}
+    </br>
+{/courseinfo}
